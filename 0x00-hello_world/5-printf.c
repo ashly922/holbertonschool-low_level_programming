4f3874f15a2899131cea73721b149da3c07f1a54
ashly922 <ashly922@yahoo.com>
2023-01-30 20:04:51 -0800

    0x00 c Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is void
+ * printf - should print the given statement
+ * Return: Always 0 (sucess)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
