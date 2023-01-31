f7003fdb7a406854fbab3256eab92ba906943322
ashly922 <ashly922@yahoo.com>
2023-01-30 20:03:06 -0800

    0x00 c Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,12 @@
-#!/bin/bash
-puts ( 'Programming is like building a multilingual puzzle' )
-
+#include <stdio.h>
+/**
+ * main - void
+ * description - this script will return zero
+ * Return: Always 0 (Success)
+ * this is a puts function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
