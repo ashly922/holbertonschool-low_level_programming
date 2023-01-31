8bdbb28cb2d920d9d8fa7e98fceb2971041ee97f
ashly922 <ashly922@yahoo.com>
2023-01-30 20:35:25 -0800

    0x01 Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int x;
+for (x = '0'; x <= '9'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
