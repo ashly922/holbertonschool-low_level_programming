bee0803b6b517f799637dc6a910f03638d5240be
ashly922 <ashly922@yahoo.com>
2023-02-06 20:49:43 -0800

    0x04 Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+	*_isupper - checks for uppercase
+	* main - check the code
+	* @a: factor
+	* @b: factor
+	* Return: product of factors
+	*/
+int mul(int a, int b)
+
+{
+	return (a*b);
+}
