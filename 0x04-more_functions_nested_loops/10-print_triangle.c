644a60562c5b81742ef6d4059a36c6cca41dd766
ashly922 <ashly922@yahoo.com>
2023-02-11 12:40:27 -0800

    0x04 Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - prints triangles on terminal
+ * @size: size of triangle
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void print_triangle(int size)
+{
+	int x, y = 0;
+	int space;
+
+	if (size > 0)
+	{
+		while (y < size)
+		{
+			space = size - y - 1;
+			while (x < size)
+			{
+				if (space > x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+				x++;
+			}
+			y++;
+			x = 0;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
