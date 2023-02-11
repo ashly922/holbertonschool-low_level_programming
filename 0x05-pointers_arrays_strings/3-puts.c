c6392d44fea62931dfa58788d32e42dd972e61a9
ashly922 <ashly922@yahoo.com>
2023-02-11 14:13:48 -0800

    0x05 Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: declaring str for puts
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != 0;)
+		x++;
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
