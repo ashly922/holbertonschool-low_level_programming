c54e61883d39201b6c91a7516595dd492bc19a7e
ashly922 <ashly922@yahoo.com>
2023-02-20 15:41:48 -0800

    0 correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints name of program
+ * @argv: argument vector
+ * @argc: number of arguments
+ * Return: (0)
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
