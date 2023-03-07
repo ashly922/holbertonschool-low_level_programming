6eb0c81c7c0ba1bec17d2c400dcb6acfe6017aa0
ashly922 <ashly922@yahoo.com>
2023-03-06 18:49:55 -0800

    0x0E Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+/*
+ * struct dog - structure for dog
+ * @name: pointer to dogs name
+ * @age: age of puppers
+ * @owner: pointer to owners name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
