7d55c927acae98ef96d2daf238375e3f31a09efb
ashly922 <ashly922@yahoo.com>
2023-03-19 10:53:01 -0700

    created function listint_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - function printd number of elements in a linked list
+ * @h: pointer to head of list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
