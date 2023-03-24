2d0dfb292132ea4f70ac29a3271a7bf09b70cc92
ashly922 <ashly922@yahoo.com>
2023-03-24 10:29:42 -0700

    created func dlistint_len

--- 0x18-doubly_linked_lists/1-dlistint_len.c
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
-/*
- * dlistint_len-measures amount of elements in a list
+/**
+ * dlistint_len - measures amount of elements in a list
  * @h: pointer to head of list
+ *
  * Return: number of elements in specified linked lists
  */
 size_t dlistint_len(const dlistint_t *h)
