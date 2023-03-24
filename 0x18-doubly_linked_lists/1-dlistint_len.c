6606a0db4b0e6697068ddfb1b47b958deb463024
ashly922 <ashly922@yahoo.com>
2023-03-24 10:19:43 -0700

    betty correction

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/*
+ * dlistint_len-measures amount of elements in a list
+ * @h: pointer to head of list
+ * Return: number of elements in specified linked lists
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
